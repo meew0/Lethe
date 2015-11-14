1e065c5665ddf11df2f2f1aa534c27c79e73c2c3
meew0 <blactbt@live.de>
2015-11-14 22:53:55 +0100

    Initial commit (pretty much)

---
+++ lethe.js
@@ -0,0 +1,142 @@
+var Discord = require('discord.js');
+
+var youtubeStream = require('youtube-audio-stream');
+var ytdl = require('ytdl-core');
+
+var shouldDisallowQueue = require('./lib/permission-checks.js');
+var VideoFormat = require('./lib/video-format.js');
+
+var client = new Discord.Client();
+
+// Handle discord.js warnings
+client.on('warn', (m) => console.log('[warn]', m));
+
+var playQueue = [];
+var boundChannel = false;
+var currentStream = false;
+var currentVideo = false;
+
+var botMention = false;
+
+client.on('ready', () => {
+  botMention = `<@${client.internal.user.id}>`;
+  console.log(`Bot mention: ${botMention}`);
+});
+
+client.on('message', m => {
+  if (!botMention) return;
+
+  if (m.content.startsWith(`${botMention} i`)) { // init
+    for (var channel of m.channel.server.channels) {
+      if (channel instanceof Discord.VoiceChannel) {
+        boundChannel = m.channel;
+        client.reply(m, `Binding to text channel <#${boundChannel.id}> and voice channel **${channel.name}** \`(${channel.id})\``);
+        client.joinVoiceChannel(channel).catch(error);
+        break;
+      }
+    }
+  }
+
+  if (m.content.startsWith(`${botMention} d`)) { // destroy
+    if (!boundChannel) return;
+    client.reply(m, `Unbinding from <#${boundChannel.id}> and destroying voice connection`);
+    playQueue = [];
+    currentStream.destroy();
+    client.internal.leaveVoiceChannel();
+    boundChannel = false;
+    return;
+  }
+
+  // Only respond to other messages inside the bound channel
+  if (!m.channel.equals(boundChannel)) return;
+
+  if (m.content.startsWith(`${botMention} n`)) { // next
+    currentStream.destroy();
+    playStopped();
+  }
+
+  if (m.content.startsWith(`${botMention} y`) // youtube
+    || m.content.startsWith(`${botMention} q`)) { // queue
+
+    var requestUrl = 'http://www.youtube.com/watch?v=' + getArgument(m.content);
+    ytdl.getInfo(requestUrl, (err, info) => {
+      if (err) handleYTError(err);
+      else possiblyQueue(info, m.author.id, m);
+    });
+  }
+
+  if (m.content.startsWith(`${botMention} l`)) { // list
+    var formattedList = 'Here are the videos currently in the play queue, from first added to last added: \n';
+    formattedList += `Currently playing: ${VideoFormat.prettyPrintWithUser(currentVideo)}\n`;
+    playQueue.forEach((video, idx) => {
+      formattedList += `${idx + 1}. ${VideoFormat.prettyPrintWithUser(video)}\n`;
+    });
+    client.reply(m, formattedList);
+  }
+});
+
+function getArgument(message) {
+  var rest = message.split(' ');
+  rest.splice(0, 2);
+  rest = rest.join(' ');
+  return rest;
+}
+
+function possiblyQueue(video, userId, m) {
+  video.userId = userId;
+  reason = shouldDisallowQueue(playQueue, video);
+  if (reason) {
+    client.reply(m, `You can't queue this video right now! Reason: ${reason}`);
+  } else {
+    playQueue.push(video);
+    client.reply(m, `Queued ${VideoFormat.prettyPrint(video)}`);
+
+    // Start playing if not playing yet
+    if (!currentVideo) nextInQueue();
+  }
+}
+
+function handleYTError(err) {
+  if (err.toString().indexOf('Code 150') > -1) {
+    // Video unavailable in country
+    client.sendMessage(boundChannel, 'This video is unavailable in the country the bot is running in! Please try a different video.');
+  } else {
+    client.sendMessage(boundChannel, 'An error occurred while getting video information! Please try a different video.');
+  }
+
+  console.log(err.toString());
+}
+
+function playStopped() {
+  if (client.internal.voiceConnection) client.internal.voiceConnection.stopPlaying();
+
+  client.sendMessage(boundChannel, `Finished playing ${VideoFormat.simplePrint(currentVideo)}`);
+  currentVideo = false;
+  nextInQueue();
+}
+
+function play(video) {
+  currentVideo = video;
+  if (client.internal.voiceConnection) {
+    var connection = client.internal.voiceConnection;
+    currentStream = youtubeStream(video.loaderUrl);
+    currentStream.on('end', () => setTimeout(playStopped, 8000)); // 8 second leeway for bad timing
+    connection.playRawStream(currentStream).then(intent => {
+      client.sendMessage(boundChannel, `Playing ${VideoFormat.prettyPrint(video)}`);
+    });
+  }
+}
+
+function nextInQueue() {
+  if (playQueue.length > 0) {
+    next = playQueue.shift();
+    play(next);
+  }
+}
+
+function error(argument) {
+  console.log(argument.stack);
+}
+
+// Email and password over command line
+client.login(process.argv[2], process.argv[3]).catch((e) => console.log(e));
