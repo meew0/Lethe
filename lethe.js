ba42e29bd44a3f6fd82d53a1759a4e69a7f94f64
meew0 <blactbt@live.de>
2015-11-30 17:28:55 +0100

    Replace the correct argument with the vid...

--- lethe.js
+++ lethe.js
@@ -238,7 +238,7 @@ client.on('message', m => {
     var requestUrl = 'http://www.youtube.com/watch?v=' + vid;
     ytdl.getInfo(requestUrl, (err, info) => {
       if (err) handleYTError(err);
-      else saveVideo(info, splitArgs[0], vid, m);
+      else saveVideo(info, vid, splitArgs[1], m);
     });
   }
 
