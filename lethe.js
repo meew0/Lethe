ca69443b45a26509694752d183e1a2fed73be1a4
meew0 <blactbt@live.de>
2015-12-27 13:46:59 +0100

    Move the uncaughtException handler to the bottom

--- lethe.js
+++ lethe.js
@@ -1,24 +1,3 @@
-process.on('uncaughtException', function(err) {
-  // Handle ECONNRESETs caused by `next` or `destroy`
-  if (err.code == 'ECONNRESET') {
-    // Yes, I'm aware this is really bad node code. However, the uncaught exception
-    // that causes this error is buried deep inside either discord.js, ytdl or node
-    // itself and after countless hours of trying to debug this issue I have simply
-    // given up. The fact that this error only happens *sometimes* while attempting
-    // to skip to the next video (at other times, I used to get an EPIPE, which was
-    // clearly an error in discord.js and was now fixed) tells me that this problem
-    // can actually be safely prevented using uncaughtException. Should this bother
-    // you, you can always try to debug the error yourself and make a PR.
-    console.log('Got an ECONNRESET! This is *probably* not an error. Stacktrace:');
-    console.log(err.stack);
-  } else {
-    // Normal error handling
-    console.log(err);
-    console.log(err.stack);
-    process.exit(0);
-  }
-});
-
 var Discord = require('discord.js');
 
 var ytdl = require('ytdl-core');
@@ -510,3 +489,24 @@ function error(argument) {
 
 // Email and password over command line
 client.login(process.argv[2], process.argv[3]).catch((e) => console.log(e));
+
+process.on('uncaughtException', function(err) {
+  // Handle ECONNRESETs caused by `next` or `destroy`
+  if (err.code == 'ECONNRESET') {
+    // Yes, I'm aware this is really bad node code. However, the uncaught exception
+    // that causes this error is buried deep inside either discord.js, ytdl or node
+    // itself and after countless hours of trying to debug this issue I have simply
+    // given up. The fact that this error only happens *sometimes* while attempting
+    // to skip to the next video (at other times, I used to get an EPIPE, which was
+    // clearly an error in discord.js and was now fixed) tells me that this problem
+    // can actually be safely prevented using uncaughtException. Should this bother
+    // you, you can always try to debug the error yourself and make a PR.
+    console.log('Got an ECONNRESET! This is *probably* not an error. Stacktrace:');
+    console.log(err.stack);
+  } else {
+    // Normal error handling
+    console.log(err);
+    console.log(err.stack);
+    process.exit(0);
+  }
+});
